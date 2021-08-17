d051d7723474b1f7b3c3c8cc2870e38031b668aa
ttw225 <peter86225@gmail.com>
2021-08-17 16:53:06 +0800

    refactor(sct013.ino): refactor read one CT sensor and fit `read_arduino_one`

--- Arduino/sct013.ino
+++ Arduino/sct013.ino
@@ -5,18 +5,15 @@ EnergyMonitor emon1;
 // Create an instance
 
 void setup() {
-  Serial.begin(9600);
-  // Current: input pin, calibration.
-  emon1.current(1, 65);
+    Serial.begin(9600);
+    // Current: input pin, calibration.
+    emon1.current(0, 60);
 }
 
 void loop() {
-  // Calculate Irms only
-  double Irms = emon1.calcIrms(1480);
-  // Apparent power
-  Serial.print(Irms);
-  Serial.print(" ");
-  Serial.println(Irms*220.0);
-  // delay for one minute
-  delay(60000);
+    // Calculate Irms only
+    double Irms = emon1.calcIrms(1480);
+    // Apparent power
+    Serial.println(Irms);
+    delay(60000);
 }
